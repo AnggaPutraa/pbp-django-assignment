69ac6fa07909c753e6c128adad04a745247e9f2f
AnggaPutraa <yananggaa@gmail.com>
2022-09-11 14:27:45 +0800

    tugas 2 pbp

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    catalog_data = CatalogItem.objects.all()
+    return render(
+        request, 
+        'katalog.html',
+        {
+            'name' : 'Angga',
+            'student_id' : '2106650065',
+            'catalogs' : catalog_data,
+        },
+    )
