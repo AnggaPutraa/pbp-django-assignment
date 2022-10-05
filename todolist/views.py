72cec3cc060b09e97e8658a33c3ab59e553e18d7
AnggaPutraa <yananggaa@gmail.com>
2022-10-06 00:06:23 +0700

    assignment 5: trigger heroku

--- todolist/views.py
+++ todolist/views.py
@@ -61,7 +61,6 @@ def login_user(request):
             username = username,
             password = password
         )
-
         if user is not None:
             login(request, user)
             response = HttpResponseRedirect(
